a37827f04e446d1afbae08e0906245b1a3dc3cb9
Jinwei Lin <jinwei.lin8@gmail.com>
2015-03-26 15:39:23 -0400

    Initial Commit

---
+++ LinJ_Project4_CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinJ_Project4_CPP
+//
+//  Created by Jinwei Lin on 3/26/15.
+//  Copyright (c) 2015 JL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
